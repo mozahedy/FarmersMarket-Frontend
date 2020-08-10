f4aa5356a30ecfd47608284be8cedbfb1d51fcf8
MichealSebre <msebre@gmail.com>
2020-08-10 10:00:33 -0500

    Front-end for fetching orders to farmer
    service and component creation for order fetch api

--- farmersmaket/src/app/farmer/orders/order.service.ts
+++ farmersmaket/src/app/farmer/orders/order.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {observable,throwError, of} from 'rxjs';
+import {catchError, retry} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
-export class OrderService {
 
-  constructor() { }
+
+export class OrderService {
+  orderListUrl='http://localhost:3000/api/farmers/pending/farmers/5f2f3159d5f2e432f096956b'
+  constructor(private http: HttpClient) { }
+
+  getOrders() :any {
+      return this.http.get<any>(this.orderListUrl)
+  }
 }
